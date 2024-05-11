ce78de61aa4a2456f33c72d4a07d9e37c7981087
naresh <Naresh@gmail.com>
2024-05-11 21:16:21 +0530

    delete old files

--- terraform/dev/backend.tf
+++
@@ -1,6 +0,0 @@
-bucket = "eternal-s3-dev"
-key    = "catalogue-jenkins"
-region = "us-east-1"
-dynamodb_table = "s3-table-dev"
-
-
--- terraform/prod/backend.tf
+++
@@ -1,4 +0,0 @@
-bucket = "eternal-s3-prod"
-key    = "catalogue-jenkins"
-region = "us-east-1"
-dynamodb_table = "s3-table-prod"
